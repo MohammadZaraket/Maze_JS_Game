aff32064d9fd307801175bf39545763e7c6f6879
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-01-22 10:52:00 +0200

    Creating Changing Colors Functions

--- game.js
+++ game.js
@@ -2,7 +2,51 @@ window.onload = function(){
 
 
 
-    document.getElementById("game").style.backgroundColor="black";
+    var start = document.getElementById("start");
+    var end = document.getElementById("end");
+
+ 
+
+
+
+    function startGame(){
+
+
+        var x= document.getElementsByClassName("boundary");
+        var i =0;
+
+        for(i; i<x.length; i++)
+
+        {
+            x[i].style.backgroundColor="#eeeeee";
+        }
+
+    };
+
+
+
+
+    function turnRed(){
+
+        
+        var x= document.getElementsByClassName("boundary");
+        var i =0;
+
+        for(i; i<x.length; i++)
+
+        {
+            x[i].style.backgroundColor="red";
+        }
+
+
+    };
+
+
+
+
+
+
+
 
 
 
